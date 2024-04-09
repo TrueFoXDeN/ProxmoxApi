e37f33a3627f0af9bede91fe6fc9766609778c03
Laurenz Dilba <laurenz.dilba@hotmail.de>
2024-04-09 18:20:04 +0200

    vm wakeup / shutdown

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ from api.auth import encode_token, token_required
 from api.exceptions import exception_handler
 from api import response_generator as r
 from wakeonlan import send_magic_packet
-import platform  # For getting the operating system name
-import subprocess  # For executing a shell command
 from proxmoxer import ProxmoxAPI
+
+from util.ping import ping
 
 app = Flask(__name__)
 cors = CORS(app)
 
 is_online = [False, False, False]
+is_online[0] = ping('192.168.140.17')
+print(f'Server is online: {is_online[0]}')
+is_online[1] = ping('192.168.140.12')
+print(f'Windows VM is online: {is_online[1]}')
+is_online[2] = ping('192.168.140.11')
+print(f'Linux VM is online: {is_online[2]}')
 
 
 @app.route('/', methods=["GET"])
@@ -56,6 +62,7 @@ def toggle(uid, node):
                             is_online[0] = False
                         else:
                             time.sleep(1)
+                    return r.respond({"status": "server shutdown complete"})
                 else:
                     # Wakeup
                     print('Starting wake on lan...')
@@ -67,29 +74,54 @@ def toggle(uid, node):
                             time.sleep(1)
                     print('Finished.')
 
-                pass
+                    return r.respond({"status": "server wakeup complete"})
+
             case 1:
                 # windows start / shutdown
-                pass
+                proxmox = ProxmoxAPI(
+                    "192.168.140.17", user="root@pam", password="Mastermind1324!-proxmox.,-", verify_ssl=False
+                )
+
+                if is_online[1]:
+                    proxmox.nodes('pve').qemu('101').status.shutdown.post()
+                    while is_online[1]:
+                        if ping('192.168.140.12'):
+                            is_online[1] = False
+                        else:
+                            time.sleep(1)
+                    return r.respond({"status": "windows vm shutdown complete"})
+                else:
+                    proxmox.nodes('pve').qemu('101').status.start.post()
+                    while not is_online[1]:
+                        if ping('192.168.140.12'):
+                            is_online[1] = True
+                        else:
+                            time.sleep(1)
+                    return r.respond({"status": "windows vm wakeup complete"})
             case 2:
-                # windows start / shutdown
-                pass
+                # linux start / shutdown
+                proxmox = ProxmoxAPI(
+                    "192.168.140.17", user="root@pam", password="Mastermind1324!-proxmox.,-", verify_ssl=False
+                )
 
-    return r.respond({"status": "ok"})
+                if is_online[2]:
+                    proxmox.nodes('pve').qemu('100').status.shutdown.post()
+                    while is_online[2]:
+                        if ping('192.168.140.11'):
+                            is_online[2] = False
+                        else:
+                            time.sleep(1)
+                    return r.respond({"status": "linux vm shutdown complete"})
+                else:
+                    proxmox.nodes('pve').qemu('100').status.start.post()
+                    while not is_online[2]:
+                        if ping('192.168.140.11'):
+                            is_online[2] = True
+                        else:
+                            time.sleep(1)
+                    return r.respond({"status": "linux vm wakeup complete"})
 
-
-def ping(host):
-    """
-    Returns True if host (str) responds to a ping request.
-    """
-
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
-
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-
-    return subprocess.call(command) == 0
+    return r.respond({"status": f"no valid node found for id={node}"}, status=400)
 
 
 if __name__ == '__main__':
---
+++ util/ping.py
@@ -0,0 +1,14 @@
+import platform  # For getting the operating system name
+import subprocess  # For executing a shell command
+def ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0
